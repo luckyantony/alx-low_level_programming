3a85ff1686f032e83f34353c7dc481b5ab8d8e9a
luckyantony <luckyantonyofficial@gmail.com>
2021-06-24 14:25:55 -0700

    Corrections

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,48 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
-* print_number - Prints an integer.
-* @n: The integer to prints.
+* print_number - print number
+* @n: input value
 *
-* Return: Nothing!
+* Description:  prints an integer
+* Return: Always (0)
 */
 void print_number(int n)
 {
-unsigned int k = n;
-if (n < 0)
+if ((n < 0) && (n >= -9))
 {
-n *= -1;
-k = n;
 _putchar('-');
+_putchar((n * -1) + '0');
 }
-k /= 10;
-if (k != 0)
-print_number(k);
-_putchar((unsigned int) n % 10 + '0');
+else if (n <= -10)
+{
+_putchar('-');
+_putchar(((n / 10) * -1) + '0');
+_putchar(((n % 10)  * -1) + '0');
+}
+else if (n == 0)
+{
+_putchar(n + '0');
+}
+else if ((n > 0) && (n <= 9))
+{
+_putchar(n + '0');
+}
+else if ((n >= 10) && (n <= 99))
+{
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+}
+else if ((n >= 100) && (n <= 999))
+{
+_putchar((n / 100) + '0');
+_putchar(((n / 10) % 10) + '0');
+_putchar((n % 10) + '0');
+}
+else if ((n >= 1000) && (n <= 9999))
+{
+_putchar((n / 1000) + '0');
+_putchar((n / 100) % 10 + '0');
+_putchar((n / 10) % 10 + '0');
+_putchar((n % 10) + '0');
+}
 }
