ccbd38aac1a8a8aaef5b292b853ead059a60fd15
luckyantony <luckyantonyofficial@gmail.com>
2021-06-24 14:18:36 -0700

    Advanced nested loops and functions

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,18 +1,22 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
-* print_number -print number putchar
-* @n:integer
-* Return: void
+* print_number - Prints an integer.
+* @n: The integer to prints.
+*
+* Return: Nothing!
 */
 void print_number(int n)
 {
-unsigned int x = n;
+unsigned int k = n;
 if (n < 0)
 {
+n *= -1;
+k = n;
 _putchar('-');
-x = -x;
 }
-if ((x / 10) > 0)
-print_number(x / 10);
-_putchar(x % 10 + '0');
+k /= 10;
+if (k != 0)
+print_number(k);
+_putchar((unsigned int) n % 10 + '0');
 }
