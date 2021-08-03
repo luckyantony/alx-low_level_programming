aa6cf84cc538ee50979ec6d573b37ce51c7cb643
luckyantony <luckyantonyofficial@gmail.com>
2021-08-03 14:07:15 -0700

    Corrections

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,36 +1,23 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 /**
-* main - check the code for Holberton School students.
-*
-* Return: Always 0.
+* reverse_listint - reverses a listint_t linked list
+* @head: double pointer to head
+* Return: address of head of list
 */
-int main(void)
+listint_t *reverse_listint(listint_t **head)
 {
-listint_t *head;
-listint_t *head2;
-listint_t *node;
-head2 = NULL;
-add_nodeint(&head2, 0);
-add_nodeint(&head2, 1);
-add_nodeint(&head2, 2);
-add_nodeint(&head2, 3);
-add_nodeint(&head2, 4);
-add_nodeint(&head2, 98);
-add_nodeint(&head2, 402);
-add_nodeint(&head2, 1024);
-print_listint_safe(head2);
-head = NULL;
-node = add_nodeint(&head, 0);
-add_nodeint(&head, 1);
-add_nodeint(&head, 2);
-add_nodeint(&head, 3);
-add_nodeint(&head, 4);
-node->next = add_nodeint(&head, 98);
-add_nodeint(&head, 402);
-add_nodeint(&head, 1024);
-print_listint_safe(head);
-return (0);
+listint_t *next_dest = NULL, *tmpd = NULL, *current = *head;
+if (!head || !(*head))
+return (*head);
+next_dest = current->next;
+current->next = NULL;
+while (next_dest)
+{
+tmpd = next_dest->next;
+next_dest->next = current;
+current = next_dest;
+next_dest = tmpd;
+}
+*head = current;
+return (*head);
 }
